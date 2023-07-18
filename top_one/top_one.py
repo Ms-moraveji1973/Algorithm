9abd371af56e48aee6731b32b8e227ddc324a12c
Mohammad Sadegh <ms.moraveji2007@gmail.com>
2023-07-18 16:14:12 +0430

    This changed to write some comments

--- top_one/top_one.py
+++ top_one/top_one.py
@@ -1,20 +1,18 @@
-"""
-  top one
-"""
-
+#created function
 def index(arr):
     value = {}
     result = []
     f_val = 0
-
+#Looping through the list we sent
     for i in arr:
         if i in value:
             value[i] += 1
         else:
             value[i] = 1
 
+#Find the highest repetition
     f_val = max(value.values())
-
+#Looping over value keys
     for i in value.keys():
         if value[i] == f_val:
             result.append(i)
@@ -25,3 +23,5 @@ def index(arr):
 
 
 print(index([4,12]))
+
+#Mohammad Sadegh
